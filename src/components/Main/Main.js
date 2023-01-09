531017aa0b5796f7fd30eb7ed5bcfa8d4e872349
Rio Edwards <rioredwards@gmail.com>
2023-01-09 13:05:36 -0800

    Add background image in main.js

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,6 +1,8 @@
-import React from 'react';
 import './Main.css';
+import { animals } from '../../data.js';
+import background from '../../background.png';
+import Animal from '../Animal/Animal.js';
 
 export default function Main() {
-  return <main></main>;
+  return <main style={{ backgroundImage: `url(${background})` }}></main>;
 }
