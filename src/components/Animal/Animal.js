984acbadd34f7ac935a0d6b41d1d7b0faa46e6c5
Rio Edwards <rioredwards@gmail.com>
2023-01-09 14:06:12 -0800

    Render animal name and says

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -4,6 +4,8 @@ export default function Animal(props) {
   return (
     <div className="animal" style={{ top: props.top, left: props.left }}>
       <img src={`${process.env.PUBLIC_URL}/animals/${props.type}.svg`} />
+      <p className="name">{props.name}</p>
+      <p>{props.says}</p>
     </div>
   );
 }
