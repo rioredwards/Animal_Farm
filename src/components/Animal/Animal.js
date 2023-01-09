9d55c3abf44dabb59a05bc878a7a012a34abc97d
Rio Edwards <rioredwards@gmail.com>
2023-01-09 14:03:54 -0800

    Render animal images

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -1,3 +1,9 @@
-export default function Animal() {
-  return <></>;
+import './Animal.css';
+
+export default function Animal(props) {
+  return (
+    <div className="animal" style={{ top: props.top, left: props.left }}>
+      <img src={`${process.env.PUBLIC_URL}/animals/${props.type}.svg`} />
+    </div>
+  );
 }
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -4,5 +4,18 @@ import background from '../../background.png';
 import Animal from '../Animal/Animal.js';
 
 export default function Main() {
-  return <main style={{ backgroundImage: `url(${background})` }}></main>;
+  return (
+    <main style={{ backgroundImage: `url(${background})` }}>
+      {animals.map((animal) => (
+        <Animal
+          key={animal.name}
+          name={animal.name}
+          type={animal.type}
+          says={animal.says}
+          top={animal.top}
+          left={animal.left}
+        />
+      ))}
+    </main>
+  );
 }
